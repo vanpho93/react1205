86c007f935a07fac34e625d2577bab83f5ed8a30
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-13 11:39:12 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Word from './components/Word';
+import Loop from './components/Loop';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,9 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Word wordInfo={{ en: 'One', vn: 'Mot', isMemorized: true }} />
+        {/* <Word wordInfo={{ en: 'One', vn: 'Mot', isMemorized: true }} />
         <Word wordInfo={{ en: 'Two', vn: 'Hai', isMemorized: false }} />
-        <Word wordInfo={{ en: 'Three', vn: 'Ba', isMemorized: true }} />
+        <Word wordInfo={{ en: 'Three', vn: 'Ba', isMemorized: true }} /> */}
+        <Loop />
       </div>
     );
   }
