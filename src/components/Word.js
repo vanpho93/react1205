e902e60952eb29a29361be3c790210dc2c67c72d
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-13 10:37:53 +0700

    Word component

---
+++ src/components/Word.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Word extends Component {
+    render() {
+        return (
+            <div>
+                <p>Word Component</p>
+            </div>
+        )
+    }
+}
+
+export default Word;
