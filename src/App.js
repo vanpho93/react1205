b493ae37af64a62525f6e314c45a8cf70a159d7a
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-20 10:39:47 +0700

    add store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import List from './components/List';
+// import List from './components/List';
+import Parent from './components/Parent';
 import './App.css';
+
+import { createStore } from 'redux';
+
+const store = createStore((state = { count: 1 }, action) => {
+  if (action.type === 'INCREASE') return { count: state.count + 1 };
+  if (action.type === 'DESCREASE') return { count: state.count - 1 };
+  if (action.type === 'RESET') return { count: 1 };
+  return state;
+});
+
+console.log(store.getState());
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'INCREASE' });
+console.log(store.getState());
+store.dispatch({ type: 'DESCREASE' });
+console.log(store.getState());
+store.dispatch({ type: 'RESET' });
+console.log(store.getState());
 
 class App extends Component {
   render() {
-    return <List />;
+    return <Parent />;
   }
 }
 
 export default App;
+
+// dinh nghia ra store √
+// ket noi store voi components √
+// hien thi du lieu tu store ra components
+// thay doi du lieu store tu components
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,35 +1,11 @@
 import React, { Component } from 'react';
 
 export default class Parent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { count: 1 };
-        this.onIncrease = this.onIncrease.bind(this);
-        this.onDescrease = this.onDescrease.bind(this);
-        this.onReset = this.onReset.bind(this);
-    }
-
-    onIncrease() {
-        this.setState({ count: this.state.count + 1 });
-    }
-
-    onDescrease() {
-        this.setState({ count: this.state.count - 1 });
-    }
-
-    onReset() {
-        this.setState({ count: 1 });
-    }
-
     render() {
         return (
             <div>
-                <h3>Value = {this.state.count}</h3>
-                <Child
-                    onIncrease={this.onIncrease}
-                    onDescrease={this.onDescrease}
-                    onReset={this.onReset}
-                />
+                <h3>Value = 0</h3>
+                <Child />
             </div>
         );
     }
@@ -37,12 +13,11 @@ export default class Parent extends Component {
 
 class Child extends Component {
     render() {
-        const { onIncrease, onDescrease, onReset } = this.props;
         return (
             <div>
-                <button className="btn btn-success" onClick={onIncrease}>Increase</button>
-                <button className="btn btn-danger" onClick={onDescrease}>Descrease</button>
-                <button className="btn btn-warning" onClick={onReset}>Reset</button>
+                <button className="btn btn-success">Increase</button>
+                <button className="btn btn-danger">Descrease</button>
+                <button className="btn btn-warning">Reset</button>
             </div>
         );
     }
